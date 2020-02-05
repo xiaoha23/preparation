d5d39476c6828b18265fdc77f32cb00db3aa455a
Xiaonan Wei CW <a9vt5zz@mmm.com>
2020-02-04 22:13:24 -0600

    lattice question init

---
+++ src/main/java/Solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public String solution(int ax, int ay, int bx, int by) {
+        int cx = 0;
+        int cy = 0;
+        int diffx = Math.abs(ax - bx);
+        int diffy = Math.abs(ay - by);
+        if (diffx == 0) {
+            cy = by;
+            cx = ay > by ? minus(bx, 1) : plus(bx, 1);
+        } else if (diffy == 0) {
+            cx = bx;
+            cy = ax > bx ? plus(by, 1) : minus(by, 1);
+        } else {
+            if (diffx % diffy == 0) {
+                diffx =Math.abs(diffx / diffy);
+                diffy = 1;
+            }
+
+            cx = ay > by ? minus(bx, diffy) : plus(bx, diffy);
+            cy = ax > bx ? plus(by, diffx) : minus(by, diffx);
+
+        }
+
+        return cx + "," + cy;
+    }
+
+    private int plus(int bValue, int offSet) {
+        return bValue + offSet;
+    }
+
+    private int minus(int bValue, int offSet) {
+        return bValue - offSet;
+    }
+}
+
---
+++ src/main/java/SolutionTest.java
@@ -0,0 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SolutionTest {
+
+    private Solution solution;
+
+    @Before
+    public void init() {
+        solution = new Solution();
+    }
+
+    @Test
+    public void test1_solution(){
+        assertEquals("2,-1", solution.solution(-1, 3, 3, 1));
+        assertEquals("2,2", solution.solution(-3, 3, 2, 3));
+        assertEquals("-3,4", solution.solution(2, 3, -3, 3));
+        assertEquals("2,2", solution.solution(3, 3, 3, 2));
+        assertEquals("4,3", solution.solution(3, 2, 3, 3));
+        assertEquals("-3,4", solution.solution(2, 3, -3, 3));
+        assertEquals("7,3", solution.solution(0, 0, 2, 5));
+    }
+}
